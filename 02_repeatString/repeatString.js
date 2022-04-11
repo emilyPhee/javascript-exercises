c45bf99f441c7bc0b69adc38b4f3a1f78da4811c
Emily  Phee <emilyphee@dhcp-v060-041.mobile.uci.edu>
2022-04-10 20:10:43 -0700

    Finish first five JavaScript Exercises

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
+const repeatString = function (string, times) {
+  let repeatedStr = '';
 
+  if (times < 0) {
+    return 'ERROR';
+  }
+
+  for (let i = 0; i < times; i++) {
+    repeatedStr += string;
+  }
+
+  return repeatedStr;
 };
 
 // Do not edit below this line
