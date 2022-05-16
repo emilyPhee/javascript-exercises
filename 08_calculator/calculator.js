e910b38bcf3a8973c727371a13da9323083071b2
Emily Phee <ephee@uci.edu>
2022-05-16 15:53:17 -0700

    Finish Fundamental 5

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,32 @@
-const add = function() {
-	
+const add = function (num1, num2) {
+  return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function (num1, num2) {
+  return num1 - num2;
 };
 
-const sum = function() {
-	
+// compute sum of numbers in array
+const sum = function (numbers) {
+  return numbers.reduce((total, num) => total + num, 0);
 };
 
-const multiply = function() {
-
+const multiply = function (numbers) {
+  return numbers.reduce((total, num) => total * num, 1);
 };
 
-const power = function() {
-	
+const power = function (num1, num2) {
+  return num1 ** num2;
 };
 
-const factorial = function() {
-	
+const factorial = function (n) {
+  if (n === 0) return 1;
+
+  let answer = 1;
+  for (let i = n; i >= 1; i--) {
+    answer *= i;
+  }
+  return answer;
 };
 
 // Do not edit below this line
@@ -29,5 +36,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
